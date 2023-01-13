c4688708ddc10d85c92e69be16368c9c2b91d7d3
fred-fittywebdev <contact@fittywebdev.fr>
2023-01-13 20:46:07 +0100

    first commit to test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Salut Frédéric
         </p>
         <a
           className="App-link"
