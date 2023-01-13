6bb34d337c3b8fd07d19868c041ada010f42b6ae
fred-fittywebdev <contact@fittywebdev.fr>
2023-01-13 21:17:10 +0100

    installation de vercel

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Salut Frédéric
+          Salut Frédéric Guerra
         </p>
         <a
           className="App-link"
