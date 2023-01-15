cc3359eccc5fb481a609a917d47058c8a0217c9d
fred-fittywebdev <contact@fittywebdev.fr>
2023-01-15 13:21:27 +0100

    ✨ F01: LOGIN > Add title and form

--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -1,5 +1,30 @@
+import { useState } from "react"
+
 export default function LoginPage() {
+    // state
+    const [prenom, setPrenom] = useState("")
+
+    // components
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        alert(`Bonjour ${prenom}`)
+        setPrenom("")
+    }
+
+    const handleChange = (event) => {
+        setPrenom(event.target.value)
+    }
+
+    // affichage (render)
     return (
-        <div>LoginPage</div>
+        <div>
+            <h1>Bienvenue chez nous !</h1>
+            <br />
+            <h2>Connectez-vous</h2>
+            <form action="submit" onSubmit={handleSubmit}>
+                <input onChange={handleChange} value={prenom} type="text" name="text" id="text" placeholder="Entrez votre prénom..." required />
+                <button>Accédez à votre espace</button>
+            </form>
+        </div>
     )
 }
