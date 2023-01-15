a608defe8bc9bf303a3b4e27ea619740967bd468
fred-fittywebdev <contact@fittywebdev.fr>
2023-01-15 13:31:35 +0100

    ♻️ F01: LOGIN > Create LoginForm

--- src/components/pages/login/LoginForm.js
+++ src/components/pages/login/LoginForm.js
@@ -1,6 +1,6 @@
-import { useState } from "react"
+import React, { useState } from 'react'
 
-export default function LoginPage() {
+export default function LoginForm() {
     // state
     const [prenom, setPrenom] = useState("")
 
@@ -11,20 +11,17 @@ export default function LoginPage() {
         setPrenom("")
     }
 
+    // affichage
     const handleChange = (event) => {
         setPrenom(event.target.value)
     }
-
-    // affichage (render)
     return (
-        <div>
+        <form action="submit" onSubmit={handleSubmit}>
             <h1>Bienvenue chez nous !</h1>
             <br />
             <h2>Connectez-vous</h2>
-            <form action="submit" onSubmit={handleSubmit}>
-                <input onChange={handleChange} value={prenom} type="text" name="text" id="text" placeholder="Entrez votre prénom..." required />
-                <button>Accédez à votre espace</button>
-            </form>
-        </div>
+            <input onChange={handleChange} value={prenom} type="text" name="text" id="text" placeholder="Entrez votre prénom..." required />
+            <button>Accédez à votre espace</button>
+        </form>
     )
 }
