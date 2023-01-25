2b41d8e2f2505b9c3de3055bb598943638b79411
fred-fittywebdev <contact@fittywebdev.fr>
2023-01-25 10:08:24 +0100

    💄 F03: LOGIN > LoginForm > Add passwordInput component

--- src/components/pages/login/LoginForm.js
+++ src/components/pages/login/LoginForm.js
@@ -3,13 +3,16 @@ import { useNavigate } from 'react-router-dom'
 import styled from 'styled-components'
 import { theme } from '../../../theme'
 import { BsPersonCircle } from 'react-icons/bs'
+// import { BsFillKeyFill } from 'react-icons/bs'
 import { IoChevronForward } from 'react-icons/io5'
 import TextInput from '../../reusable-ui/TexInput'
+// import PasswordInput from '../../reusable-ui/PasswordInput'
 import PrimaryButton from '../../reusable-ui/PrimaryButton'
 
 export default function LoginForm() {
     // state
     const [prenom, setPrenom] = useState("")
+    const [password, setPassword] = useState("")
     // Forcer la redirection lorsque on entre le prénom ( penser a modifier l'url dans Route sur app.js)
     const navigate = useNavigate()
 
@@ -17,13 +20,23 @@ export default function LoginForm() {
     const handleSubmit = (event) => {
         event.preventDefault()
         setPrenom("")
-        navigate(`order/${prenom}`)
+        if (password === "admin") {
+            navigate(`order/${prenom}`)
+            setPassword("")
+        }
+
     }
 
     // affichage
     const handleChange = (event) => {
         setPrenom(event.target.value)
     }
+
+    // const passwordChange = (event) => {
+    //     setPassword(event.target.value)
+    // }
+
+
     return (
         <LoginFormStyled action="submit" onSubmit={handleSubmit}>
             <div>
@@ -33,6 +46,7 @@ export default function LoginForm() {
             </div>
             <div>
                 <TextInput value={prenom} onChange={handleChange} placeholder={"Entrez votre prénom"} required Icon={<BsPersonCircle className='icon' />} />
+                {/* <PasswordInput type={"password"} onChange={passwordChange} value={password} placeholder={"Entrez votre mot de passe"} required Icon={<BsFillKeyFill className='icon' />} /> */}
                 <PrimaryButton label={"Accéder à mon espace"} Icon={<IoChevronForward className='icon' />} />
             </div>
         </LoginFormStyled>
