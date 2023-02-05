a56d374104ee2c7e31de4048b18e0ad4af7775a6
fred-fittywebdev <contact@fittywebdev.fr>
2023-02-05 11:30:03 +0100

    ✨ FO4: ORDER > Navbar > Add profile componenent

--- src/components/pages/order/NavbarRight.js
+++ src/components/pages/order/NavbarRight.js
@@ -1,20 +1,27 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import styled from 'styled-components';
+import Profile from './Profile';
 
 export default function NavbarRight({ prenom }) {
     return (
         <NavbarRightStyled>
-            Right
-            <h1>Bonjour {prenom} </h1>
-            <Link to='/'>
-                <button>Déconnexion</button>
-            </Link>
+            {/*<div className="admin-btn">Admin button</div>*/}
+            <Profile />
         </NavbarRightStyled>
     )
 }
 
 const NavbarRightStyled = styled.div`
-  background-color: purple;
+  display: flex;
+  align-items: center;
+  padding-right: 50px;
 
+    /*.admin-btn {
+        background-color: lightblue;
+    }*/
+
+    .profile {
+        background-color: yellow;
+    }
 `;
